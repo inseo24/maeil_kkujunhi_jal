86ca4cf04e18cfdf7a8530c1a5e82cd4ddf8cf4b
inseo24 <84627144+inseo24@users.noreply.github.com>
2023-01-01 14:08:18 +0900

    :memo: binarySearch in python

---
+++ Algorithm/binarySearch/index.py
@@ -0,0 +1,25 @@
+def binarySearch(arr, target):
+  left, right = 0, len(arr) - 1
+  
+  while left <= right:
+    mid = (left + right) // 2
+    
+    if arr[mid] == target:
+      return mid
+    elif arr[mid] < target:
+      left = mid + 1
+    else:
+      right = mid - 1
+  
+  return -1
+      
+arr = [1,2,3,4,5,6] 
+target = 6
+
+result = binarySearch(arr, target) # index : 5
+
+if result != -1:
+  print("answer : %d" % result)
+else:
+  print("no answer")
+  
